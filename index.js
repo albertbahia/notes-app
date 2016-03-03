6112d3475d05e5e479245b98337147657b3bf981
albertbahia <albert.bahia1@gmail.com>
2016-03-03 01:23:37 -0500

    started using body-parser to encode the form data in the url

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var router = require('./routes/router');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
+
+/********************************************/
+/****** Middleware to Encode Form Data ******/
+/********************************************/
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/********************************************/
+/**********        Router          **********/
+/********************************************/
 app.use('/', router);
 
 var PORT = process.env.PORT || 8080;
